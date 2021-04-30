d3a71a825f0e7c514f7bfa8ae0c8e17ba5444466
micafer <micafer1@upv.es>
2021-04-30 09:20:56 +0200

    Changes to run at k8s

--- badgr-ui/run.sh
+++ badgr-ui/run.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-# Script to enable to use ENV variable to set the URL of the badgr server
+# Script to enable to use ENV variable to set the URL of the badgr server and the base dir
 sed -i 's#http:\/\/badgrserverhost:8000#'"$BADGRSERVER"'#g' /usr/share/nginx/html/main.*.js
+
+# Set the basedir
+sed -i 's#<base href=\/ >#<base href='$BASE_DIR' >#g' /usr/share/nginx/html/index.html
 
 nginx -g "daemon off;"
